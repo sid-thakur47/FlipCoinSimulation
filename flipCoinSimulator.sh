4ea3ecbd6e66417d74982b00fa70d473b96c19c8
sid-thakur47 <sidthakur258@gmail.com>
2020-04-02 13:01:26 +0530

    ADDED:To store combination and find percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,51 @@
-HEAD=1
-random=$(($(($RANDOM%2))))
-if [ $random -eq $HEAD ]
-	then
-		echo "Toss result is Head"
-	else
-		echo "Toss result is Tail"
-fi
+PERCENT=100
+
+read -p "Enter number of flips:" flip
+
+function generateRandom() {
+	random=$(($(($RANDOM%2))))
+	if [ $random -eq 1 ]
+		then
+			echo "H"
+		else
+			echo "T"
+	fi
+}
+
+function getCombination() {
+	if [ $flipSide -eq 1 ]
+		then
+			coinFace="$(generateRandom)"
+	fi
+	echo $coinFace
+}
+
+function calculatePercentage() {
+	percent=$(($(($currentValue*$PERCENT))/$flip))
+	echo $percent
+}
+
+function getPercentage() {
+	for i in "${!coinToss[@]}"
+		do
+			currentValue=${coinToss[$i]}
+			headTailPercent="$(calculatePercentage)"
+			echo "percentage of $i side is $headTailPercent %"
+	done
+}
+
+function main() {
+	declare -A coinToss
+	for ((i=1;i<=$flip;i++))
+		do
+			combination="$(getCombination)"
+			coinToss[$combination]=$((${coinToss[$combination]}+1))
+	done
+	echo "Sides:"${!coinToss[*]} 
+	echo "Times:"${coinToss[*]}
+	getPercentage
+	unset coinToss
+}
+
+flipSide=1
+main
