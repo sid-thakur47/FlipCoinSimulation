2527d224ea5eec4b647ecd8a5d1946a71c35629e
sid-thakur47 <sidthakur258@gmail.com>
2020-04-02 18:50:27 +0530

    ADDED:Function to get random value

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,44 +1,48 @@
 PERCENT=100
-read -p "Enter number of flips:" flip
+read -p "Enter number of flips:" FLIP
 
 function generateRandom() {
-random=$(($(($RANDOM%2))))
+	random=$(($(($RANDOM%2))))	
+}
+
+function coinToss() {
+generateRandom
 	if [ $random -eq 1 ]
-		then
-			echo "H"
-		else
-			echo "T"
+	then
+		echo "H"
+	else
+		echo "T"
 	fi
 }
 
 function getCombination() {
 	if [ $flipSide -eq 1 ]
-		then
-			coinFace="$(generateRandom)"
+	then
+			coinFace="$(coinToss)"
 	fi
 	echo $coinFace
 }
 
 function calculatePercentage() {
-	percent=$(($(($currentValue*$PERCENT))/$flip))
+	percent=$(($(($currentValue*$PERCENT))/$FLIP))
 	echo $percent
 }
 
 function getPercentage() {
 	for i in "${!coinToss[@]}"
-		do
-			currentValue=${coinToss[$i]}
-			headTailPercent="$(calculatePercentage)"
-			echo "percentage of $i side is $headTailPercent %"
+	do
+		currentValue=${coinToss[$i]}
+		headTailPercent="$(calculatePercentage)"
+		echo "percentage of $i side is $headTailPercent %"
 	done
 }
 
 function main() {
 	declare -A coinToss
-	for ((i=1;i<=$flip;i++))
-		do
-			combination="$(getCombination)"
-			coinToss[$combination]=$((${coinToss[$combination]}+1))
+	for ((i=1;i<=$FLIP;i++))
+	do
+		combination="$(getCombination)"
+		coinToss[$combination]=$((${coinToss[$combination]}+1))
 	done
 	echo "Sides:"${!coinToss[*]} 
 	echo "Times:"${coinToss[*]}
