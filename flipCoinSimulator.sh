38e52fc5730059f8ba8ae312adae1e338cd98d22
sid-thakur47 <sidthakur258@gmail.com>
2020-04-03 11:23:41 +0530

    REFACTORED:Changed Indentation

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -6,13 +6,13 @@ function generateRandom() {
 }
 
 function coinToss() {
-generateRandom
-	if [ $random -eq 1 ]
-	then
-		echo "H"
-	else
-		echo "T"
-	fi
+	generateRandom
+		if [ $random -eq 1 ]
+		then
+			echo "H"
+		else
+			echo "T"
+		fi
 }
 
 function getCombination() {
@@ -35,43 +35,40 @@ function calculatePercentage() {
 
 function getPercentage() {
 	for i in "${!coinToss[@]}"
-	do
+		do
 		currentValue=${coinToss[$i]}
 		headTailPercent="$(calculatePercentage)"
 		echo "percentage of $i side is $headTailPercent %"
 	done
-	}
+}
 
 function winningCombination() {
 	arrLargest=($(echo ${coinToss[*]}|   tr " " "\n" | sort -nr))
 	for k in "${!coinToss[@]}"
 	do
-	if [ ${arrLargest[0]} -eq ${coinToss[$k]} ] 
-	then
-		echo "Winning Combination is:"$k
-	fi
+		if [ ${arrLargest[0]} -eq ${coinToss[$k]} ] 
+		then
+			echo "Winning Combination is:"$k
+		fi
 	done
 }
 
-function main() {
+function computeCoinToss() {
 	declare -A coinToss
-	for ((i=1;i<=$FLIP;i++))
-	do
-		combination="$(getCombination)"
-		coinToss[$combination]=$((${coinToss[$combination]}+1))
-	done
-	echo "Sides:"${!coinToss[*]} 
-	echo "Times:"${coinToss[*]}
-	winningCombination
-	getPercentage
-	unset coinToss
+		for ((i=1;i<=$FLIP;i++))
+			do
+			combination="$(getCombination)"
+			coinToss[$combination]=$((${coinToss[$combination]}+1))
+		done
+		echo "Sides:"${!coinToss[*]} 
+		echo "Times:"${coinToss[*]}
+		winningCombination
+		getPercentage
+		unset coinToss
 }
-
-function start() {
-	for ((j=1;j<=3;j++))
-	do
-		flipSide=$j
-		main
-	done
-}
-start
+flipSide=1
+computeCoinToss
+flipSide=2
+computeCoinToss
+flipSide=3
+computeCoinToss
